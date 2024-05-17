e5c722b2711ea95630ef9e18d339ab4a0cb153bb
Anjie Le <ale-ale@qq.com>
2024-05-17 12:07:41 +0100

    loss update, integration not working

--- calculate_loss.py
+++ calculate_loss.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.integrate import quad
 from functools import partial
+from get_stats import get_stats
 
 
 def Ti(mu_Y_N, mu_Y_C, sigma_Y_N, sigma_Y_C, n, m, sigma_v):
@@ -13,7 +14,6 @@ def Ti(mu_Y_N, mu_Y_C, sigma_Y_N, sigma_Y_C, n, m, sigma_v):
 
 
 def T_Y_given_Z(
-    z,
     mu_Y_N,
     mu_Y_C,
     sigma_Y_N,
@@ -27,6 +27,7 @@ def T_Y_given_Z(
     n,
     m,
     sigma_v,
+    z,
 ):
     # Numerator
     term1 = mu_Y_N + rho_N * ((z - mu_Z_N) / sigma_Z_N) * sigma_Y_N
@@ -42,13 +43,13 @@ def T_Y_given_Z(
     return numerator / denominator
 
 
-def f_Z_N(z, mu_Z_N, sigma_Z_N):
+def f_Z_N(mu_Z_N, sigma_Z_N, z):
     return (1 / np.sqrt(2 * np.pi * sigma_Z_N**2)) * np.exp(
         -((z - mu_Z_N) ** 2) / (2 * sigma_Z_N**2)
     )
 
 
-def f_Z_C(z, mu_Z_C, sigma_Z_C):
+def f_Z_C(mu_Z_C, sigma_Z_C, z):
     return (1 / np.sqrt(2 * np.pi * sigma_Z_C**2)) * np.exp(
         -((z - mu_Z_C) ** 2) / (2 * sigma_Z_C**2)
     )
@@ -63,26 +64,64 @@ def Mij(
     return integral_result
 
 
+def get_params(stats):
+    num_genes = stats["num_genes"]
+    mu_N = stats["mu_N"]
+    mu_C = stats["mu_C"]
+    sigma_N = stats["sigma_N"]
+    sigma_C = stats["sigma_C"]
+    rho_N = stats["rho_N"]
+    rho_C = stats["rho_C"]
+    p_N = stats["p_N"]
+    p_C = stats["p_C"]
+    sigma_v = stats["sigma_v"]
+    n = stats["n"]
+    m = stats["m"]
+
+    v = np.zeros(num_genes)
+    W = np.zeros((num_genes, num_genes))
+
+    # [ ] may want to optimize these for-loops in the future to speed up code
+    for i in range(num_genes):
+        v[i] = Ti(mu_N[i], mu_C[i], sigma_N[i], sigma_C[i], n, m, sigma_v)
+
+    for j in range(num_genes):
+        for k in range(num_genes):
+            T_Y_given_Z_as_z = partial(
+                T_Y_given_Z,
+                mu_N[j],
+                mu_C[j],
+                sigma_N[j],
+                sigma_C[j],
+                rho_N[j, k],
+                rho_C[j, k],
+                mu_N[k],
+                mu_C[k],
+                sigma_N[k],
+                sigma_C[k],
+                n,
+                m,
+                sigma_v,
+            )
+            f_Z_N_as_z = partial(f_Z_N, mu_N[k], sigma_N[k])
+            f_Z_C_as_z = partial(f_Z_C, mu_C[k], sigma_C[k])
+
+            Z_upper = max(mu_N[k] + 3 * sigma_N[k], mu_C[k] + 3 * sigma_C[k])
+            Z_lower = min(mu_N[k] - 3 * sigma_N[k], mu_C[k] - 3 * sigma_C[k])
+            Z_limits = (Z_lower, Z_upper)
+
+            W[j, k] = Mij(
+                T_Y_given_Z_as_z, p_N, p_C, f_Z_N_as_z, f_Z_C_as_z, Z_limits
+            )
+
+    return v, W
+
+
 if __name__ == "__main__":
 
-    # Constants
-    P_N = 0.5
-    P_C = 0.5
-    Z_limits = (-np.inf, np.inf)
+    base_case = "simulated_data/simulated_data_case0.csv"
+    comparing_case = "simulated_data/simulated_data_case1.csv"
 
-    # Calculate M_ij
-    M_ij = calculate_Mij(T_Y_given_Z, P_N, P_C, f_Z_N, f_Z_C, Z_limits)
-    print(f"M_ij: {M_ij}")
-
-    # Values for T_i calculation
-    mu_Y_N = 1.0
-    mu_Y_C = 0.5
-    sigma_Y_N = 1.0
-    sigma_Y_C = 1.0
-    n = 30
-    m = 30
-    sigma_v = 0.1
-
-    # Calculate T_i
-    T_i = calculate_Ti(mu_Y_N, mu_Y_C, sigma_Y_N, sigma_Y_C, n, m, sigma_v)
-    print(f"T_i: {T_i}")
+    stats = get_stats(base_case, comparing_case)
+    v, W = get_params(stats)
+    print(v.shape, W.shape)
--- get_stats.py
+++ get_stats.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
 
-def load_data_and_calculate_means(base_case, comparing_case):
+def get_stats(base_case, comparing_case):
     # Load the data from the CSV files
     base_data = pd.read_csv(base_case)
     comparing_data = pd.read_csv(comparing_case)
+
+    num_genes = len(base_data.columns)
+    assert len(base_data.columns) == len(
+        comparing_data.columns
+    ), "They don't have the same number of genes!"
 
     # get the length of the data
     n = len(base_data)
@@ -13,27 +18,43 @@ def load_data_and_calculate_means(base_case, comparing_case):
     assert m == 50, "The number of samples should be 50"
 
     # Calculate the sample means for each row
-    mu_N = base_data.mean(axis=0)
-    mu_C = comparing_data.mean(axis=0)
+    mu_N = base_data.mean(axis=0).to_numpy()
+    mu_C = comparing_data.mean(axis=0).to_numpy()
     assert len(mu_N) == 1000
     assert len(mu_C) == 1000
 
-    sigma_N = base_data.std(axis=0)
-    sigma_C = comparing_data.std(axis=0)
+    sigma_N = base_data.std(axis=0).to_numpy()
+    sigma_C = comparing_data.std(axis=0).to_numpy()
 
     # calculate sample correlations
-    rho_N = base_data.corr()
-    rho_C = comparing_data.corr()
-    print(rho_N.shape)
+    rho_N = base_data.corr().to_numpy()
+    rho_C = comparing_data.corr().to_numpy()
 
-    return
+    p_N = n / (n + m)
+    p_C = m / (n + m)
+
+    sigma_v = 0.084
+
+    stats = {
+        "num_genes": num_genes,
+        "n": n,
+        "m": m,
+        "mu_N": mu_N,
+        "mu_C": mu_C,
+        "sigma_N": sigma_N,
+        "sigma_C": sigma_C,
+        "rho_N": rho_N,
+        "rho_C": rho_C,
+        "p_N": p_N,
+        "p_C": p_C,
+        "sigma_v": sigma_v,
+    }
+
+    return stats
 
 
 if __name__ == "__main__":
     # Load the data and calculate the sample means
     file1 = "simulated_data/simulated_data_case0.csv"
     file2 = "simulated_data/simulated_data_case1.csv"
-    means1, means2 = load_data_and_calculate_means(file1, file2)
-
-    print(f"Means for case0: {means1.values}")
-    print(f"Means for case1: {means2.values}")
+    stats = get_stats(file1, file2)
