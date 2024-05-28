fix bug in corr calculation, recalculate v W

calculate_v_W.py
@@ -12,7 +12,7 @@ def Ti(mu_Y_N, mu_Y_C, sigma_Y_N, sigma_Y_C, n, m, sigma_v):
         np.sqrt((sigma_Y_N**2 + sigma_Y_C**2) * (1 / n + 1 / m) / (n + m - 2))
         + sigma_v
     )
-    return numerator / denominator
+    return abs(numerator / denominator)
 
 
 def T_Y_given_Z(
@@ -42,6 +42,29 @@ def T_Y_given_Z(
     ) * (1 / n + 1 / m)
     denominator = np.sqrt(denominator_inner / (n + m - 2)) + sigma_v
 
+    result = numerator / denominator
+    if not np.isfinite(result):
+        print(
+            result,
+            numerator,
+            denominator,
+            term1,
+            term2,
+            z,
+            mu_Z_N,
+            mu_Z_C,
+            mu_Y_C,
+            mu_Y_N,
+            sigma_Y_N,
+            sigma_Y_C,
+            rho_N,
+            rho_C,
+            n,
+            m,
+            sigma_v,
+        )
+        raise ValueError("T_Y_given_Z is not finite")
+
     return numerator / denominator
 
 
@@ -70,22 +93,27 @@ def Mij(
     # Generate a range of z values
     z_values = np.linspace(Z_limits[0], Z_limits[1], 1000)
 
-    # Calculate the function values
-    y_values = [integrand(z) for z in z_values]
+    # check if integral_result is a number
+    if np.isnan(integral_result):
 
-    # Plot the function
-    # plt.figure(figsize=(10, 6))
-    # plt.plot(z_values, y_values)
-    # plt.xlabel("z")
-    # plt.ylabel("integrand(z)")
-    # plt.title("PDF of z")
-    # plt.grid(True)
-    # plt.show()
+        # Calculate the function values
+        y_values = [integrand(z) for z in z_values]
 
-    return integral_result
+        # Plot the function
+        plt.figure(figsize=(10, 6))
+        plt.plot(z_values, y_values)
+        plt.xlabel("z")
+        plt.ylabel("integrand(z)")
+        plt.title("PDF of z")
+        plt.grid(True)
+        plt.show()
+
+        raise ValueError("Integral result is NaN")
+
+    return abs(integral_result)
 
 
-def get_params(stats, save=True):
+def get_params(stats, save=False):
     num_genes = stats["num_genes"]
     mu_N = stats["mu_N"]
     mu_C = stats["mu_C"]
@@ -104,17 +132,17 @@ def get_params(stats, save=True):
 
     # [ ] may want to optimize these for-loops in the future to speed up code
     for i in range(num_genes):
-        v[i] = Ti(mu_N[i], mu_C[i], sigma_N[i], sigma_C[i], n, m, sigma_v)
+        v[i] = abs(Ti(mu_N[i], mu_C[i], sigma_N[i], sigma_C[i], n, m, sigma_v))
 
     if save:
         os.makedirs("output", exist_ok=True)
-        np.save(f"output/v_{save}.npy", v)
+        np.save(f"output/v_W/v_{save}.npy", v)
 
     for j in range(num_genes):
         if j % 1 == 0:
             print(f"Processing gene {j} of {num_genes}")
             if save:
-                np.save(f"output/W_{save}.npy", W)
+                np.save(f"output/v_W/W_{save}.npy", W)
         for k in range(num_genes):
             T_Y_given_Z_as_z = partial(
                 T_Y_given_Z,
@@ -139,17 +167,25 @@ def get_params(stats, save=True):
             Z_lower = min(mu_N[k] - 3 * sigma_N[k], mu_C[k] - 3 * sigma_C[k])
             Z_limits = (Z_lower, Z_upper)
 
-            W[j, k] = Mij(
-                T_Y_given_Z_as_z, p_N, p_C, f_Z_N_as_z, f_Z_C_as_z, Z_limits
+            W[j, k] = abs(
+                Mij(
+                    T_Y_given_Z_as_z,
+                    p_N,
+                    p_C,
+                    f_Z_N_as_z,
+                    f_Z_C_as_z,
+                    Z_limits,
+                )
             )
 
+    print(f"{save} Done!")
     return v, W
 
 
 if __name__ == "__main__":
 
     base_case = "simulated_data/simulated_data_case0.csv"
-    for i in range(2, 5):
+    for i in range(1, 5):
         comparing_case = f"simulated_data/simulated_data_case{i}.csv"
         stats = get_stats(base_case, comparing_case)
         v, W = get_params(stats, save=f"case{i}")

genetic_algorithm.py
@@ -1,12 +1,26 @@
 import numpy as np
 import pygad
-from hyperparameters import LAMBDA
+
 from functools import partial
 
 
 # Define the objective function
-def fit_func(lambda_, W, v, ga_instance, solution, solution_idx):
+def fit_func(W, v, ga_instance, solution, solution_idx):
+    ###!! Calculating lambda, this method is only valid when no PPI is considered
+
+    mean_v = np.mean(v)
+    sd_v = np.std(v)
+
+    # for W, calculate row mean
+    mean_W = np.mean(W)
+    sd_W = np.std(W)
+    M_v = mean_v / sd_v
+    M_w = mean_W / sd_W
+    lambda_ = M_w / (M_v + M_w)
+    print(f"Lambda: {lambda_}")
+
     x = np.array(solution)
+
     term1 = lambda_ * np.dot(x.T, np.dot(W, x))
     term2 = (1 - lambda_) * np.dot(v.T, x)
     loss = term1 + term2
@@ -24,46 +38,51 @@ def on_generation(ga_instance):
 
 
 if __name__ == "__main__":
+    for idx in range(1, 5):
+        W = np.load(f"output/sparse_v_W/W_case{idx}.npy")
+        v = np.load(f"output/sparse_v_W/v_case{idx}.npy")
+        fitness_func = lambda ga_instance, solution, solution_idx: fit_func(
+            W, v, ga_instance, solution, solution_idx
+        )
 
-    W = np.load("W_case1.npy")
-    v = np.load("v_case1.npy")
-    fitness_func = lambda ga_instance, solution, solution_idx: fit_func(
-        LAMBDA, W, v, ga_instance, solution, solution_idx
-    )
+        # GA parameters
+        num_iterations = 10000  # Set the number of iterations # 60000
+        num_generations = (
+            num_iterations  # Assuming one generation per iteration
+        )
+        num_parents_mating = 10
+        sol_per_pop = 20
+        num_genes = len(W)  # Ensure num_genes matches the dimension of W and v
+        mutation_rate = 1 / (num_genes + 1)  # Set mutation rate
+        crossover_rate = 0.5  # Set crossover rate
 
-    # GA parameters
-    num_iterations = 60000  # Set the number of iterations
-    num_generations = num_iterations  # Assuming one generation per iteration
-    num_parents_mating = 10
-    sol_per_pop = 20
-    num_genes = len(W)  # Ensure num_genes matches the dimension of W and v
-    mutation_rate = 1 / (num_genes + 1) + 1  # Set mutation rate
-    crossover_rate = 0.5  # Set crossover rate
+        # Creating an instance of the GA
+        ga_instance = pygad.GA(
+            num_generations=num_generations,
+            num_parents_mating=num_parents_mating,
+            fitness_func=fitness_func,
+            sol_per_pop=sol_per_pop,
+            num_genes=num_genes,
+            mutation_type="random",
+            mutation_percent_genes=20,  # pygad uses percentage
+            crossover_type="single_point",  # Experiment with different crossover methods
+            crossover_probability=crossover_rate,  # Set crossover rate
+            on_generation=on_generation,
+            stop_criteria=[
+                "saturate_100"
+            ],  # Stop if no improvement for 100 generations
+            keep_parents=1,
+            initial_population=np.random.uniform(
+                low=0.0, high=1.0, size=(sol_per_pop, num_genes)
+            ),  # Ensure initial population in [0,1]
+        )
+        # Running the GA
+        ga_instance.run()
 
-    # Creating an instance of the GA
-    ga_instance = pygad.GA(
-        num_generations=num_generations,
-        num_parents_mating=num_parents_mating,
-        fitness_func=fitness_func,
-        sol_per_pop=sol_per_pop,
-        num_genes=num_genes,
-        mutation_type="random",
-        mutation_percent_genes=20,  # pygad uses percentage
-        crossover_type="single_point",  # Experiment with different crossover methods
-        crossover_probability=crossover_rate,  # Set crossover rate
-        on_generation=on_generation,
-        stop_criteria=[
-            "saturate_100"
-        ],  # Stop if no improvement for 100 generations
-        keep_parents=1,
-        initial_population=np.random.uniform(
-            low=0.0, high=1.0, size=(sol_per_pop, num_genes)
-        ),  # Ensure initial population in [0,1]
-    )
-    # Running the GA
-    ga_instance.run()
+        # Best solution
+        solution, solution_fitness, solution_idx = ga_instance.best_solution()
+        print("Best Solution: ", solution)
+        print("Best Solution Fitness: ", solution_fitness)
 
-    # Best solution
-    solution, solution_fitness, solution_idx = ga_instance.best_solution()
-    print("Best Solution: ", solution)
-    print("Best Solution Fitness: ", solution_fitness)
+        # Save the best solution
+        # np.save(f"output/best_solution_case{idx}.npy", solution)